e9360a930fadf32bb3c7d33481ae7816691dcd62
Isabayweb <isabayweb@gmail.com>
2025-01-11 06:59:23 -0300

    Update db.php

--- db.php
+++ db.php
@@ -4,12 +4,6 @@ $username = "isabayweb";
 $password = "rpsyfvlsor01";
 $dbname = "lugox";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error al conectar a la base de datos: " . $e->getMessage());
-}
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
